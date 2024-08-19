a3f00369ac4a721a3f809980d6864de1ac51ec8b
Mima <andreasoares38@gmail.com>
2024-08-19 19:28:24 +0200

    Ajout de la barre de recherche pour les livres

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -3,20 +3,39 @@
 namespace App\Controller;
 
 use App\Entity\Book;
+use App\Form\SearchBookType;
+use App\Model\SearchData;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
 class BookController extends AbstractController
 {
     #[Route('/', name: 'book_list')]
-    public function index(EntityManagerInterface $entityManager): Response
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
-        $books = $entityManager->getRepository(Book::class)->findAll();
+        // Création du formulaire de recherche
+    $searchData = new SearchData();
+    $searchForm = $this->createForm(SearchBookType::class, $searchData);
 
-        return $this->render('book/index.html.twig', ['books' => $books]);
+    $searchForm->handleRequest($request);
+    if ($searchForm->isSubmitted() && $searchForm->isValid()) {
+            dd($searchData);
+        }
+
+    // Récupérer tous les livres depuis la base de données
+    $books = $entityManager->getRepository(Book::class)->findAll();
+    
+    // Rendre la vue avec le formulaire de recherche et les livres
+    return $this->render('book/index.html.twig', [
+        'books' => $books,
+        'search_form' => $searchForm->createView(),  // Transmet le formulaire au template
+    ]);
+
     }
+
 
     #[Route('/book/{id}', name: 'book_detail')]
     public function detail(Book $book): Response
@@ -24,3 +43,4 @@ class BookController extends AbstractController
         return $this->render('book/detail.html.twig', ['book' => $book]);
     }
 }
+
---
+++ src/Form/SearchBookType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Model\SearchData;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SearchBookType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('q', TextType::class, [
+                'attr' => [
+                    'placeholder' => 'Entrez le titre du livre...'
+                ]
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => SearchData::class,
+            'method' => 'GET',
+            'csrf_protection' => false
+        ]);
+    }
+}
