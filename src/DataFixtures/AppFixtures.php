b4d87448e3974c4af5caf7c2b87ba07b67f0c8b7
Mima <andreasoares38@gmail.com>
2024-08-30 15:46:36 +0200

    Creation Formulaire de login

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,16 +3,25 @@
 namespace App\DataFixtures;
 
 use App\Entity\Book;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    private UserPasswordHasherInterface $hasher;
+    public function __construct(UserPasswordHasherInterface $hasher)
+    {
+        $this->hasher = $hasher;
+    }
+    
     public function load(ObjectManager $manager)
     {
         $faker = Factory::create();
 
+        // --- BOOKS------------------------------------------------
         for ($i = 0; $i < 10; $i++) {
             $book = new Book();
             $book->setTitle($faker->sentence())
@@ -24,6 +33,25 @@ class AppFixtures extends Fixture
             $manager->persist($book);
         }
 
-        $manager->flush();
+                // --- USERS ---------------------------------------------------
+                $admin = new User();
+                $admin
+                    ->setEmail("admin@test.com")
+                    ->setRoles(["ROLE_ADMIN"])
+                    ->setPassword($this->hasher->hashPassword($admin, "admin1234"));
+        
+                $manager->persist($admin);
+
+
+                $user = new User();
+                $user
+                    ->setEmail("user@test.com")
+                    ->setPassword($this->hasher->hashPassword($user, "test1234")); // Hachage du mot de passe
+                    
+                $manager->persist($user); // Enregistre l'utilisateur dans la base de données
+
+
+                $manager->flush(); // Sauvegarde tout
+
     }
 }
